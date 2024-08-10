4997e42a438700873981f179ffd608a9915751d7
sojitradrashti <sojitradrashti5@gmail.com>
2024-08-10 05:42:57 -0700

    commit

---
+++ src/components/MenuItems.jsx
@@ -0,0 +1,20 @@
+export const MenuItems = [
+    {
+      title: "Home",
+      url: "/",
+    },
+    {
+      title: "About",
+      url: "/about",
+    },
+    {
+      title: "Shop",
+      url: "/shop",
+    },
+    {
+      title: "Contact",
+      url: "/contact",
+    },
+    
+  ];
+  
