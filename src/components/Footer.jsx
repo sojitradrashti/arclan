e1e0e85d5787eb5d1b785d2bf92438103396c412
sojitradrashti <sojitradrashti5@gmail.com>
2024-08-11 23:37:34 -0700

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
-
+import React from "react";
+import { CiFacebook } from "react-icons/ci";
+import { FaTelegram } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaFigma } from "react-icons/fa";
 const Footer = () => {
-    return (
-        <div className='footer'>
-            <div className='footer1'>
-                <div className='footer2'>
-                    <img src="./assets/logo.png" alt='image'/>
-                    <p>Hign level experience in web design and<br/>
-                    development knowledge, producing quality work. </p>
-                </div>
-                <div className='footer3'></div>
-                <div className='footer4'></div>
-                <div className='footer5'></div>
-                <div className='footer6'></div>
-            </div>
+  return (
+    <div className="footer">
+      <div className="footer1">
+        <div className="footer2">
+          <h1>Arclan</h1>
+          <p>
+            Hign level experience in web design and
+            <br />
+            development knowledge, producing quality work.
+          </p>
         </div>
-    );
-}
+        <div className="footer3">
+            <h1>Follow us</h1>
+            <span><CiFacebook /></span>
+            <span><FaTelegram /></span>
+            <span><FaInstagram /></span>
+            <span><FaFigma /></span>
+            <p>Call us <br/> +1 800 854-36-80</p>
+        </div>
+        <div className="footer4"></div>
+        <div className="footer5"></div>
+        <div className="footer6"></div>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
